25760d29b760226e0090ed28e88c9b4b7e8f53c5
Akshay <akshaythappar9@gmail.com>
2023-02-06 14:21:41 +0530

    added pricing section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Nav from './components/commons/Nav';
 import Portfolio from './components/home/Portfolio';
 import Footer from './components/commons/Footer';
 import Contact from './components/home/Contact';
+import Pricing from './components/home/Pricing';
 
 function App() {
   return (
     <>
       <Nav />
       <Portfolio />
+      <Pricing />
       <Contact />
       <Footer />
     </>
--- src/components/commons/Footer.js
+++ src/components/commons/Footer.js
@@ -4,21 +4,21 @@ const Footer = () => {
     return (
         <>
             <footer id="footer">
-                <div class="container">
-                    <div class="row d-flex align-items-center">
-                        <div class="col-lg-6 text-lg-left text-center">
-                            <div class="copyright">
+                <div className="container">
+                    <div className="row d-flex align-items-center">
+                        <div className="col-lg-6 text-lg-left text-center">
+                            <div className="copyright">
                                 &copy; Copyright <strong>Vesperr</strong>. All Rights Reserved
                             </div>
-                            <div class="credits">
+                            <div className="credits">
 
                                 Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
                             </div>
                         </div>
-                        <div class="col-lg-6">
-                            <nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
-                                <a href="#intro" class="scrollto">Home</a>
-                                <a href="#about" class="scrollto">About</a>
+                        <div className="col-lg-6">
+                            <nav className="footer-links text-lg-right text-center pt-2 pt-lg-0">
+                                <a href="#intro" className="scrollto">Home</a>
+                                <a href="#about" className="scrollto">About</a>
                                 <a href="#">Privacy Policy</a>
                                 <a href="#">Terms of Use</a>
                             </nav>
