0d704d35db151df08d726e64213f9cf4c4b823e4
Akshay <akshaythappar9@gmail.com>
2023-02-06 13:41:07 +0530

    contact and footer updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Nav from './components/commons/Nav';
 import Portfolio from './components/home/Portfolio';
+import Footer from './components/commons/Footer';
+import Contact from './components/home/Contact';
 
 function App() {
   return (
     <>
       <Nav />
       <Portfolio />
+      <Contact />
+      <Footer />
     </>
   );
 }
